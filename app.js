f515169e1084b185b46cc2751d4c5935c653ff30
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-10-04 00:56:43 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Welcome to the Subscription Tracker API.");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	console.log(`Subscription Tracker API is running on: http://localhost:${PORT}`);
+});
