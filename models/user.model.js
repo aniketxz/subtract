4aa5e0ff8c2381c4b004c0bf3abf6c4e02b4735d
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-10-04 17:36:21 +0530

    created subscription and user models

---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "User name is required"],
+			trim: true,
+			minLength: 2,
+			maxLength: 25,
+		},
+		email: {
+			type: String,
+			required: [true, "User email is required"],
+			unique: true,
+			trim: true,
+			lowerCase: true,
+			match: [/\S+@\S+\.\S+/, "Please fill a valid email address"],
+		},
+		password: {
+			type: String,
+			required: true,
+			minLength: 6,
+		},
+	},
+	{ timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
