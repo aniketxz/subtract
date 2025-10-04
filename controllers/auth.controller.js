38b62c9d1e90ab25a9f895dfe1da0294514ba09a
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-10-04 21:52:06 +0530

    added authorization middleware

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
 
 export const signUp = async (req, res, next) => {
