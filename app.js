65274760d4cae1658bd8ebc5011f8f3b988b4f98
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-10-05 03:11:00 +0530

    integrated arcjet for rate limiting and bot protection

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { PORT } from "./config/env.js";
 
+import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import connectToDatabase from "./database/mongodb.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
-import connectToDatabase from "./database/mongodb.js";
-import errorMiddleware from "./middlewares/error.middleware.js";
-import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
---
+++ config/arcjet.js
@@ -0,0 +1,21 @@
+import arcjet, { detectBot, shield, tokenBucket } from "@arcjet/node";
+
+const aj = arcjet({
+	key: process.env.ARCJET_KEY,
+	rules: [
+		{ mode: "allow", conditions: [{ ip: "127.0.0.1" }] },
+		shield({ mode: "LIVE" }),
+		detectBot({
+			mode: "LIVE",
+			allow: ["CATEGORY:SEARCH_ENGINE", "CURL", "POSTMAN"],
+		}),
+		tokenBucket({
+			mode: "LIVE",
+			refillRate: 5, // Refill 5 tokens per interval
+			interval: 10, // Refill every 10 seconds
+			capacity: 10, // Bucket capacity of 10 tokens
+		}),
+	],
+});
+
+export default aj;
